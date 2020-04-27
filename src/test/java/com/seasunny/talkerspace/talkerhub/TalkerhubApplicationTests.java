354e07ef6405d6f01ce58511701aa79b0822adf5
cuiting <sea_sunny1229@163.com>
2020-04-28 07:43:58 +0800

    init project

---
+++ src/test/java/com/seasunny/talkerspace/talkerhub/TalkerhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seasunny.talkerspace.talkerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkerhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
